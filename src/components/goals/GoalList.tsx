ce08bae078983cda06c070490db570e47d6319b2
Gideon Cameron <gideonwork20@gmail.com>
2025-07-24 23:46:13 -0700

    Created the GoalList.tsx file

---
+++ src/components/goals/GoalList.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Goal } from '../../types/Goal';
+import GoalCard from './GoalCard';
+
+interface GoalListProps {
+  goals: Goal[];
+  onComplete: (id: string) => void;
+  onDelete: (id: string) => void;
+}
+
+const GoalList: React.FC<GoalListProps> = ({ goals, onComplete, onDelete }) => {
+  if (goals.length === 0) {
+    return (
+      <div className="text-center text-gray-500 py-8">
+        No goals set yet. Add one to get started!
+      </div>
+    );
+  }
+
+  return (
+    <div className="grid gap-4">
+      {goals.map((goal) => (
+        <GoalCard
+          key={goal.id}
+          goal={goal}
+          onComplete={onComplete}
+          onDelete={onDelete}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default GoalList;
