61f15ca7131d75c510f1de39599b41835640963a
Gideon Cameron <gideonwork20@gmail.com>
2026-07-20 01:14:17 -0700

    Creating the Ranking system

---
+++ src/components/RankingSystem/index.ts
@@ -0,0 +1,6 @@
+export { default as RankingSystem } from "./RankingSystem";
+export { default as RankBadge } from "./RankBadge";
+export { default as RankDetails } from "./RankDetails";
+
+export * from "./rankData";
+export * from "./types";
