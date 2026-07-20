d178912f5bf86defa496d362e63ea587af5cdea6
Gideon Cameron <gideonwork20@gmail.com>
2026-07-20 02:13:02 -0700

    Finished the features section

---
+++ src/components/Features/index.ts
@@ -0,0 +1,5 @@
+export { default as Features } from "./Features";
+export { default as FeatureCard } from "./FeatureCard";
+
+export { features } from "./featureData";
+export type { Feature } from "./types";
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import { RankingSystem } from "../components/RankingSystem";
+import { Features } from "../components/Features";
 
 export default function LandingPage() {
   return (
     <div className="bg-[#0A192F]">
       <Hero />
       <RankingSystem />
+      <Features />
     </div>
   );
 }
