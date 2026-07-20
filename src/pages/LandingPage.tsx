33c837aef2a0596764d06e156d87f2774b932780
Gideon Cameron <gideonwork20@gmail.com>
2026-07-20 01:30:04 -0700

    First version of the rank display

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import { RankingSystem } from "../components/RankingSystem";
 
 export default function LandingPage() {
   return (
     <div className="bg-[#0A192F]">
       <Hero />
+      <RankingSystem />
     </div>
   );
 }
